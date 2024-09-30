7ad235a033193292521fcdc0011bb7fda24ac928
lexagreat <9608798010@mail.ru>
2024-09-30 23:33:56 +0300

    make checkout

---
+++ dist/js/app.js
@@ -0,0 +1,50 @@
+(function isWebP() {
+   function testWebP(callback) {
+      let webP = new Image();
+      webP.onload = webP.onerror = function () {
+         callback(webP.height == 2);
+      };
+      webP.src =
+         "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+   }
+   testWebP(function (support) {
+      if (support == true) {
+         document.querySelector("html").classList.add("webp");
+      } else {
+         document.querySelector("html").classList.add("no-webp");
+      }
+   });
+})();
+const maskOptions = {
+   mask: "+{7} (000) 000-00-00",
+   // lazy: false,  // make placeholder always visible
+   // placeholderChar: '0'     // defaults to '_'
+};
+if (document.querySelectorAll("[data-phone]").length) {
+   document.querySelectorAll("[data-phone]").forEach((item) => {
+      const mask = IMask(item, maskOptions);
+   });
+}
+document.addEventListener("DOMContentLoaded", () => {
+   // makeAutoCompleteInput();
+});
+function tabs(linkSelector, contentSelector) {
+   const inputs = document.querySelectorAll(linkSelector);
+   const contents = document.querySelectorAll(contentSelector);
+   let value;
+   if (inputs.length) {
+      inputs.forEach((item) => {
+         item.addEventListener("change", () => {
+            if (item.checked) {
+               value = item.value;
+            }
+            contents.forEach((item) => {
+               item.classList.remove("active");
+               if (item.getAttribute("data-tab") == value) {
+                  item.classList.add("active");
+               }
+            });
+         });
+      });
+   }
+}
